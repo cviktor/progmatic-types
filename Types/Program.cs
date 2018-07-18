2e0d2fe8b9968b291e618fda46f8b56f2b8d45fb
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-18 11:35:39 +0200

    Érték típus alapok

--- Types/Program.cs
+++ Types/Program.cs
@@ -10,8 +10,19 @@ namespace Types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 3;
+            int y = x; //Az érték másolódik ezért ha x-et módosítom akkor y értéke nem változik majd
+            //Person p = new Person();
+            //p.Name = "Józsi";
+            Write(x);
+            Console.WriteLine(x);
             Console.ReadKey();
+        }
+
+        static void Write(int a)
+        {
+            a = 4;
+            Console.WriteLine(a);
         }
     }
 }
