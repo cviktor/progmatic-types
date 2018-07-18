e23ec937a22691170c2ba201bb7ab117ccd9d7c7
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-18 11:41:38 +0200

    Referencia típus példa

--- Types/Program.cs
+++ Types/Program.cs
@@ -10,13 +10,24 @@ namespace Types
     {
         static void Main(string[] args)
         {
-            int x = 3;
-            int y = x; //Az érték másolódik ezért ha x-et módosítom akkor y értéke nem változik majd
-            //Person p = new Person();
-            //p.Name = "Józsi";
-            Write(x);
-            Console.WriteLine(x);
+            //Érték típus példa
+            //int x = 3;
+            //int y = x; //Az érték másolódik ezért ha x-et módosítom akkor y értéke nem változik majd
+            //Write(x);
+            //Console.WriteLine(x);
+
+            //Referencia típus példa
+            Person p = new Person();
+            p.Name = "Józsi";
+            WritePerson(p);
+            Console.WriteLine(p.Name);
             Console.ReadKey();
+        }
+
+        static void WritePerson(Person x)
+        {
+            x.Name = "Béla";
+            Console.WriteLine(x.Name);
         }
 
         static void Write(int a)
